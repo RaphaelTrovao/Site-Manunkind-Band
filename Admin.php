b9070044650a5e2dc3fe3c026d9c09de8f97a615
RaphaelTrovao <raphaeltrovao123@gmail.com>
2026-04-06 22:05:20 -0300

    Implementação de sistema de adicionar shows com php

--- Admin.php
+++ Admin.php
@@ -12,8 +12,11 @@
         
             <button class="botIn">Adicionar Evento</button>
             <form action="back.php" method="POST" enctype="multipart/form-data">
-                <input type="text" name="data">
-                <input type="file" name="imgem" required>
+                Nome do evento: <input type="text" name="nome">
+                Data do evento: <input type="text" name="data" required>
+                Local do evento: <input type="text" name="local" required>
+                Link do evento: <input type="text" name="link" required>
+                Foto do evento: <input type="file" name="imgem" required>
                 <button type="submit">enviar</button>
             </form>
             <button class="botIn">Remover Evento</button>
